0c0173525bddd62210adfca30ba2ec980cc8bb7b
bobo88 <yuanboi88@163.com>
2023-03-22 10:28:34 +0800

    feat: 增加百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,6 @@ module.exports = {
       "script",
       {},
       `
-      <script>
         var _hmt = _hmt || [];
         (function() {
           var hm = document.createElement("script");
@@ -22,7 +21,6 @@ module.exports = {
           var s = document.getElementsByTagName("script")[0];
           s.parentNode.insertBefore(hm, s);
         })();
-        </script>
     `,
     ],
   ],
